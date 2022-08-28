5e9f729058adceb3761ce53043c5c36dd78d2afc
Roberto Rosati <24945128+bioruffo@users.noreply.github.com>
2022-08-28 15:39:31 -0300

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -18,24 +18,8 @@ from .out import print_search_results, clipboardwiki, wikitext
     
 if __name__ == '__main__':
 
-    # print("Getting remote data...")
-    # get_all_files()
-    
-    print("Creating the search engine...")
-    search_engine = SearchEngine() 
-    
-    print()
-    searchfile = "config/fruit.txt"
-    print(searchfile)
-    test = search_engine.search_group(searchfile)
-    #test = search_engine.search_item("Spear")
-    print_search_results(test)
-    
-   
-    # wikitext()
-    
-    # df = search_engine.build_droptable()
-    # df.to_csv("Item_search_chances.tsv", sep="\t")
+    print("Getting remote data...")
+    get_all_files()
 
     
                     
